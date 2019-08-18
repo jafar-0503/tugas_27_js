9c1e3d09feb4f9174f5d774de11af3aec2ffc6ca
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-18 22:53:40 +0700

    tugas_27_js
    tugas sudah ok !!!

---
+++ tugas-27.js
@@ -0,0 +1,18 @@
+function panggilfungsi(){
+
+  var a = "No"
+  var b = " rumah saya ";
+  var c = 30;
+  var d = "A"
+
+  var e = "Jalan pangeran Tirtayasa No";
+  var f = 70;
+  //
+  hasila= String(a) + String(b) + String(c) + String(d);
+  console.log("Hasil Pertama:", hasila);
+
+  hasilb = String(e) + String(f);
+  console.log("Hasil kedua:", hasilb);
+
+}
+panggilfungsi();
